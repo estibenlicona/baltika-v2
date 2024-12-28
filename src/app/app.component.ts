f806d55314ffd3eeafb8b108e3aeb3d979b197c2
estibenlicona <estibenlicona@gmail.com>
2024-12-28 03:08:47 -0500

    menu responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports:[MenuComponent]
 })
 export class AppComponent {
   title = 'baltika';
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+})
+export class MenuComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
