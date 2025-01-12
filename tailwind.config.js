bad0160af68ca324b9f3888aa53df28c0b3aba1f
estibenlicona <estibenlicona@gmail.com>
2025-01-11 20:32:08 -0500

    se agrega la configuracion de columnas

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ export default {
     extend: {
       colors: {
         primary: 'rgb(252 211 77 / var(--tw-bg-opacity, 1))',
+      },
+      gridTemplateColumns: {
+        'positions-lg': '6px 40px 40px 1fr 40px 40px 40px 40px 40px 40px 40px 40px',
+        'positions-sm': '6px 40px 40px 1fr 40px 40px 40px',
+        'positions-head-lg': '1fr repeat(8, 40px)',
+        'positions-head-sm': '1fr repeat(3, 40px)',
       }
     },
   },
