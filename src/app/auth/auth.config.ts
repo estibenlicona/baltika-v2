e6d3d3dced6e4e5ef387c6d6cd62c12b5e549032
estibenlicona <estibenlicona@gmail.com>
2025-01-13 19:16:14 -0500

    Se ajusta la redireccion para prod

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -4,7 +4,7 @@ export const authConfig: PassedInitialConfig = {
 
   config: {
     authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_y3DKBQYxn',
-    redirectUrl: 'http://localhost:4200',
+    redirectUrl: 'https://www.baltikeishon.com',
     clientId: '1h48tnu588hrlpa1qtb5mc0v6h',
     scope: 'email openid phone',
     responseType: 'code'
