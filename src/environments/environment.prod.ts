7ed6642455bba8eab24d9952a56ad67f1bfe40c6
estibenlicona <estibenlicona@gmail.com>
2025-02-27 21:01:57 -0500

    ajuste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   api: 'https://635tighcqh.execute-api.us-east-1.amazonaws.com',
-  redirect: 'https://master.d560kcg5zzlds.amplifyapp.com'
+  redirect: 'www.myclub.baltikeishon.com'
 };
