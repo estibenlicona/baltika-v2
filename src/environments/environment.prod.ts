8a341098a5caf7af0bc5df69ea8fb95293d14721
estibenlicona <estibenlicona@gmail.com>
2025-02-27 20:18:34 -0500

    reset enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   api: 'https://635tighcqh.execute-api.us-east-1.amazonaws.com',
-  redirect: 'https://myclub.baltikeishon.com'
+  redirect: 'https://www.baltikeishon.com'
 };
