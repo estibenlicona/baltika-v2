d8824c128307a0dc7bb015ae056e490aff555436
estibenlicona <estibenlicona@gmail.com>
2025-03-07 18:21:19 -0500

    se elimina el modulo de admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { PositionsComponent } from './components/positions/positions.component';
 import { FixtureComponent } from './components/fixture/fixture.component';
-import { AdminComponent } from './components/admin/admin.component';
-import { authAdminGuard, authGuard } from './auth/auth.guard';
+import { authGuard } from './auth/auth.guard';
 import { MyclubComponent } from './components/myclub/myclub.component';
 
 export const routes: Routes = [
-    { path: 'admin', component: AdminComponent, canActivate: [authAdminGuard] },
     { path: 'myclub', component: MyclubComponent, canActivate: [authGuard] },
     { path: 'positions', component: PositionsComponent  },
     { path: 'fixture', component: FixtureComponent  },
